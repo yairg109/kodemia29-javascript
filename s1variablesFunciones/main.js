deb861b3e0f82bfb382a960a6a7c53d4be4a346c
yair ramirez <yairramirezgaona@gmail.com>
2023-10-12 21:35:04 -0600

    programa-variables

---
+++ s1variablesFunciones/main.js
@@ -0,0 +1,27 @@
+console.log("Mi primer programa")
+
+//CREACION DE VARIABLES
+
+let numer1 = 10
+let numero2 = 20
+
+//SUMA
+let suma = numer1 + numero2
+console.log("Mi resultado de suma " + suma)
+
+//RESTA 
+let resta = numero2 - numer1
+console.log("Mi resta es " + resta)
+
+//Multiplicacion 
+let mult = numer1 * numero2
+//Puedo concatenar con una coma que genial
+console.log("Mi multiplicacion es ", mult)
+
+//DIVISION
+let div = numero2 / numer1
+console.log("Mi division ",div)
+
+//MODULO 
+let modulo = numero2 % numer1
+console.log("Mi modulo es ",modulo)
