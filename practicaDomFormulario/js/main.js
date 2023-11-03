45a8ccae55142eef0980eb7b244e1167ec5c1e99
yair <yairramirezgaona@gmail.com>
2023-11-02 23:40:07 -0600

    fetch

--- practicaDomFormulario/js/main.js
+++ practicaDomFormulario/js/main.js
@@ -1,27 +1,29 @@
-
 //LA LISTA DE USUARIOS
-let usersList=[]
+let usersList = [];
 
-const pasarInformacion= ()=>{
-    let name = document.getElementById("name").value
-    let email = document.getElementById("email").value
-    let foto = document.getElementById("foto").value
-    let id = Date.now().toString()
+const pasarInformacion = () => {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let foto = document.getElementById("foto").value;
+  let id = Date.now().toString();
 
-    let user = {name,email,foto,id}
-    return user
-}
+  let user = { name, email, foto, id };
+  return user;
+};
 //METODO DE DOM PARA LA SELECCION DEL ELEMENTO EN HTML "GUARDAR"
 //getElementById("nombre") seleccion del elemento por ID
 
+let btnGuardar = document.getElementById("btn-guardar");
 
-let btnGuardar = document.getElementById("btn-guardar") 
+//ACTIVAR el elemento DOM
+btnGuardar.addEventListener("click", () => {
+  usersList.push(pasarInformacion());
 
-//ACTIVAR el elemento DOM 
-btnGuardar.addEventListener("click",()=>{
+  console.log(usersList);
+});
 
-    usersList.push(pasarInformacion())
-    console.log(usersList)
-})
-
-
+const printUsers = (array) => {
+  array.array.forEach((element) => {
+    
+  });
+};
